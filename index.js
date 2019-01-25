2ff02079a5be6deb629741d09c1825f483c6a187
Daniel Ors <do7811@gmail.com>
2019-01-25 12:57:45 -0500

    completed all tasks for random images and breed image

--- index.js
+++ index.js
@@ -5,11 +5,10 @@
 $('.js-dog-pictures-form').submit(function(event){
   event.preventDefault();
   const inputValue = $('.js-number-input').val();
-    
 
   for (let i = 1; i <= inputValue; i++) {
     console.log('For loop running'); 
-    fetch('https://dog.ceo/api/breeds/image/random')
+    fetch('https://dog.ceo/api/breeds/image/random')  
       .then( response => response.json() )
       .then( data => {
         console.log(data);
@@ -17,7 +16,17 @@ $('.js-dog-pictures-form').submit(function(event){
         $('.js-dog-image-grid').prepend( `<img src="${data.message}" class="dog-picture" />` );
       }); 
   }
-});  
-// const els = data.map( repo => `<li>${repo.name}</li>` );
-// $('.results').html(els);
-// $('.results').prepend(`<p>${data.length}</p>`)
+});
+
+$('.js-dog-breed-form').submit(function(event){
+  event.preventDefault();
+  const breedValue = $('.js-breed-input').val();
+  console.log(breedValue);
+  fetch(`https://dog.ceo/api/breed/${breedValue}/images/random`) 
+    .then( response => response.json() )
+    .then( data => {
+      console.log(data);
+      console.log(data.message);
+      $('.js-dog-image-grid').prepend( `<img src="${data.message}" class="dog-picture" />` );
+    });
+});
