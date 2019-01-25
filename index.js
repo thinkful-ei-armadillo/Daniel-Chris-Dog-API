abcb26f1103c953ae01b832a629b49032ea08086
Christopher <christopher.carnivale@gmail.com>
2019-01-25 12:22:23 -0500

    displays multiple images

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
 /* eslint-disable no-console */
 'use strict';
 
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then( response => response.json() )
-  .then( data => {
-    console.log(data);
-    console.log(data.message);
-    $('.js-dog-image-grid').html( `<img src="${data.message}" class="dog-picture" />` );
-    // const els = data.map( repo => `<li>${repo.name}</li>` );
-    // $('.results').html(els);
-    // $('.results').prepend(`<p>${data.length}</p>`);
-  });
+$('.js-dog-pictures-form').submit(function(event){
+  event.preventDefault();
+  const inputValue = $('.js-number-input').val();
+    
+
+  for (let i = 1; i <= inputValue; i++) {
+    console.log('For loop running'); 
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then( response => response.json() )
+      .then( data => {
+        console.log(data);
+        console.log(data.message);
+        $('.js-dog-image-grid').prepend( `<img src="${data.message}" class="dog-picture" />` );
+      }); 
+  }
+});  
+// const els = data.map( repo => `<li>${repo.name}</li>` );
+// $('.results').html(els);
+// $('.results').prepend(`<p>${data.length}</p>`)
